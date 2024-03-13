284edaabff422d4b05958c44d0f39cdb489c42b3
nikitagowardhan <nikitagowar02@gmail.com>
2024-03-14 00:56:53 +0530

    Hibernate_Annotation

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,10 @@
-package model;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import model.Student;
 
 public class Test {
 
